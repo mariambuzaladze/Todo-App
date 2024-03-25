e490913794b165b73608492c4eb1c6743eac6175
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-25 23:34:27 +0400

    add and style filters component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Create from "./components/Create/Create";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
     <div className="content">
       <Header />
       <Create />
+      <Filters/>
     </div>
   );
 }
---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,9 @@
+import "./filters.css"
+
+export default function Filters(){
+    return <div className="filters">
+        <p className="text">All</p>
+        <p className="text">Active</p>
+        <p className="text">Completed</p>
+    </div>
+}
